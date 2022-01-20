fda3c8e5464e6b11a368a0926923b71ceff2314c
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2022-01-20 19:59:57 +0200

    Initial commit

---
+++ Data Types and Variables/Data Types and Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data_Types_and_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            decimal kilo = meters / 1000.0M;
+            Console.WriteLine($"{kilo:f2}");
+        }
+
+    }
+}
