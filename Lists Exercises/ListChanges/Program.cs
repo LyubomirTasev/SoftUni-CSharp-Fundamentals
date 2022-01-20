15c907bd046463c8cd300ced8db1137e92bf811e
Lyubomir Tasev <lubomirtasev_1998@abv.bg>
2022-01-20 20:22:20 +0200

    Initial commit

---
+++ Lists Exercises/ListChanges/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListChanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfInts = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandArgs = command.Split();
+                string firstCommand = commandArgs[0];
+                int element = int.Parse(commandArgs[1]);
+
+                if (firstCommand == "Delete")
+                {
+                    listOfInts.RemoveAll(x => x == element);
+                }
+                else if (firstCommand == "Insert")
+                {
+                    int index = int.Parse(commandArgs[2]);
+                    listOfInts.Insert(index, element);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", listOfInts));
+        }
+    }
+}
---
+++ Lists Exercises/Lists Exercises/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lists_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] commandArgs = command.Split(); // превръщаме стринга command в масив от стрингове !!!
+
+                if (commandArgs[0] == "Add")
+                {
+                    wagons.Add(int.Parse(commandArgs[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(commandArgs[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int currentWagon = wagons[i];
+                        bool isEnoughSpace = currentWagon + passengers <= maxCapacity;
+
+                        if (isEnoughSpace)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+
+        }
+    }
+}
